eb35dfa94218854341a9b4a87f46fc9604fbc58d
IndigoWizard <microclubist@gmail.com>
2022-12-25 16:47:51 +0100

    Header: Navbar update 2
    - fixed the menu icon animation when open/closed
    - fixed open menu layout; now takes whole screen with bigger font and spaced out menu options
    - fixed website logo's z-index to be on top
    - changed hover effect for menu options
    ----
    - deleted test files (nav.html/css/main.js)

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let navlinks = document.querySelector('.navlinks');
-
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
-}
